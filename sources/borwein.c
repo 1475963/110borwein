2c22dd1017d7ae7913013954fc5d7c037dc7df9f
1475963 <benzahramarc@yahoo.fr>
2013-04-09 13:40:09 +0200

    fuck you simpson

--- sources/borwein.c
+++ sources/borwein.c
@@ -5,7 +5,7 @@
 ** Login   <benzah_m@epitech.net>
 ** 
 ** Started on  Mon Apr  8 15:24:36 2013 marc benzahra
-** Last update Tue Apr  9 13:19:30 2013 marc benzahra
+** Last update Tue Apr  9 13:39:50 2013 marc benzahra
 */
 
 #include "../includes/borwein.h"
@@ -59,7 +59,7 @@ void		trapezes(double n)
   double	a = 0;
   double	b = 5000;
   double	i = a;
-  double	h = (b - a) / (2 * 10000);
+  double	h = (b - a) / 10000;
   double	result = 0;
 
   i = 1;
@@ -68,12 +68,33 @@ void		trapezes(double n)
       result = result + produit(n, i);
       i = i + h;
     }
-  result = ((result * 2) + produit(n, a) + produit(n, b)) * h;
+  result = ((result * 2)/* + produit(n, a) */+ produit(n, b)) * ((b - a) / 20000);
   prompt(result, result - (M_PI / 2), n, 2);
 }
 
 void		simpson(double n)
 {
+  double	a = 0;
+  double	b = 5000;
+  double	i = a;
+  double	h = (b - a) / 10000;
+  double	result = 0;
+  double	res = 0;
+  double	final;
+
+  while (i < b)
+    {
+      result = result + produit(n, i + (h / 2));
+      i = i + h;
+    }
+  i = 1;
+  while (i < b)
+    {
+      res = res + produit(n, i);
+      i = i + h;
+    }
+  final = ((result * 4) + (res * 2)/* + produit(n, a) */+ produit(n, b)) * ((b - a) / 60000);
+  prompt(final, final - (M_PI / 2), n, 3);
 }
 
 void		borwein(double n)
